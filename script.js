5a66999c119594d136c0694db990457d080d96a8
Vinayak Nawdhar <123320183+VinayakNawdhar@users.noreply.github.com>
2024-01-28 07:25:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("body"),
+    smooth: true,
+    // for tablet smooth
+    tablet: { smooth: true },
+    // for mobile
+    smartphone: { smooth: true }
+});
